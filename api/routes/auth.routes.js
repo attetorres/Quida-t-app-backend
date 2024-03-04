71d15bb7c3e914b51f5d733d2dbc7c989bcf57d9
Aitor Díaz Santana <aitordd.ss@gmail.com>
2024-03-04 12:12:00 +0000

    userModel change and synced

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { signUp, login }  = require('../controlls/auth.controller')
+const { signUp, login }  = require('../controllers/auth.controller')
 
 
 router.post('/signup', signUp)
--- db/sync.js
+++ db/sync.js
@@ -3,7 +3,7 @@ const UserModel = require('../api/models/user.model')
 
 const dbSync = async () => {
     try {
-        //await UserModel.sync()
+        //await UserModel.sync({force: true})
     } catch (error) {
         console.log(error)
         throw new Error(error)
