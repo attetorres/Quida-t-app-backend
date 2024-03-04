712cb8d49c9ebd99f264dfdbdd3bdbccce39111d
Fran Arteaga <fran.am8@gmail.com>
2024-03-04 16:45:30 +0000

    User.model CRUD created

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -16,4 +16,106 @@ const getAllUsers = async (req, res) => {
     }
 }
 
-module.exports = { getAllUsers }
+const getOneUser = async (req, res) => {
+    try {
+        const user = await UserModel.findByPk(req.params.id)
+        if (user) {
+            return res.stauts(200).json(user)
+        } else {
+            return res.stauts(404).send('User not found')
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error.message)
+    }
+}
+
+const updateUser = async (req, res) => {
+    try {
+        const user = await UserModel.findOne( {
+            where: {
+                email: res.locals.user.email    
+            }
+        })
+
+       /*
+        * Setting the varibles we dont want to be updated, to the original value
+        */
+       
+        req.body.validate = user.validation
+        req.body.role = user.role
+        req.body.psychologist = user.psychologist
+
+        if (!user) return res.status(500).send('User not found')
+
+        const result = await UserModel.update(req.body, {
+            where: {
+                email: res.locals.user.email
+            }
+        }) 
+
+        return res.status(200).json(await UserModel.findByPk(user.id))
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error.message)
+    }
+}
+
+const updateUserPsycho = async () => {
+    try {
+       
+        const [userExist, user] = await UserModel.update(req.body.psychologist, {
+            where: {
+                id: req.params.id 
+            }
+        }) 
+
+        return res.status(200).json(user)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error.message)
+    }
+}
+
+
+
+const deleteUser = async () => {
+    try {
+        const user = await UserModel.destroy({
+            where: {
+                email: res.locals.email,
+            }
+        })
+
+        if (user) {
+            return res.stauts(200).json('User deleted')
+        } else {
+            return res.stauts(404).send('User not found')
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error.message)
+    }
+}
+
+const psychoStatusRole = async () => {
+    try {
+        const [userExist, user] = await UserModel.update(req.body.psychologist, req.body.validation, {
+            where: {
+                id: req.params.id 
+            }
+        }) 
+
+        if (user) {
+            return res.stauts(200).json('Psychologist validated \n' + user)
+        } else {
+            return res.stauts(404).send('User not found')
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error.message)
+    }
+}
+
+module.exports = { getAllUsers, getOneUser, updateUser, updateUserPsycho, deleteUser, psychoStatusRole }
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,8 +1,15 @@
 const router = require('express').Router()
 
-const { getAllUsers } = require('../controllers/user.controller')
+const { getAllUsers, getOneUser, updateUser, updateUserPsycho, deleteUser, psychoStatusRole } = require('../controllers/user.controller')
+const { checkPsycho, checkAdmin } = require('../middlewares/checkAuth.middleware')
 
+router.get('/', checkPsycho, getAllUsers)
+router.get('/:userId', checkPsycho,  getOneUser)
 
-router.get('/', getAllUsers)
+router.put('/', updateUser)
+router.put('/:userId', checkPsycho, updateUserPsycho)
+router.put('/:userId', checkAdmin, psychoStatusRole)
+
+router.delete('/', deleteUser)
 
 module.exports = router
