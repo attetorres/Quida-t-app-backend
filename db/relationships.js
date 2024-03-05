89b201fd77ebd775168ecf60bab458faf59bb1f4
Fran Arteaga <fran.am8@gmail.com>
2024-03-05 12:19:39 +0000

    TASK: model created, controlled structured, routes structured

--- db/relationships.js
+++ db/relationships.js
@@ -1,10 +1,14 @@
 const UserModel = require('../api/models/user.model')
 const ListModel = require('../api/models/list.model')
+const TaskModel = require('../api/models/task.model')
 
 const addRelationships = () => {
     try {
         UserModel.hasMany(ListModel)
         ListModel.belongsTo(UserModel)
+
+        ListModel.hasMany(TaskModel)
+        TaskModel.belongsTo(ListModel)
 
         
     } catch (error) {
