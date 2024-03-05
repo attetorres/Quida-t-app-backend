569b2724e39793102a06247b2b35ab9183a83c1c
Fran Arteaga <fran.am8@gmail.com>
2024-03-05 16:29:34 +0000

    LIST
    updateList & deleteList only if userId === res.locals.user.id

--- api/routes/index.js
+++ api/routes/index.js
@@ -8,7 +8,7 @@ const TaskRouter = require('./task.routes')
 const { checkAuth, checkAdmin, checkPsycho } = require('../middlewares/checkAuth.middleware')
 
 router.use('/auth', AuthRouter)
-router.use('/user/list', checkAuth, TaskRouter)
+router.use('/user/list/', checkAuth, TaskRouter)
 router.use('/user/lists', checkAuth, ListRouter)
 router.use('/users', checkAuth, UserRouter)
 
