fb5872c308ac796d10e48b1b4ebb636432124e1c
attetorres <atteneritorresgonzalez@gmail.com>
2024-03-05 11:30:05 +0000

    createdList function done

--- api/controllers/list.controller.js
+++ api/controllers/list.controller.js
@@ -2,13 +2,13 @@ const ListModel = require('../models/list.model')
 
 const createList = async (req, res) => {
     try {
+        const list = await ListModel.create(req.body)
+        res.status(200).json({message:'List created', list: list})
         
     } catch (error) {
         console.log(error)
         res.status(500).send(error.message)
-        
     }
-
 }
 
 const getAllLists = async (req, res) => {
--- api/routes/index.js
+++ api/routes/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router()
 
 const AuthRouter = require('./auth.routes')
 const UserRouter = require('./user.routes')
+const ListRouter = require ('./list.routes')
 
 const { checkAuth, checkAdmin, checkPsycho } = require('../middlewares/checkAuth.middleware')
 
 router.use('/auth', AuthRouter)
 router.use('/users', checkAuth, UserRouter)
+router.use('/users/list', checkAuth, ListRouter)
 
 module.exports = router
