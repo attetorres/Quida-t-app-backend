b263d368016a8b73afce106196e58b2c9ac6d5af
Aitor Díaz Santana <aitordd.ss@gmail.com>
2024-03-05 15:24:35 +0000

    registryTask.model and sync

--- db/relationships.js
+++ db/relationships.js
@@ -1,15 +1,19 @@
 const UserModel = require('../api/models/user.model')
 const ListModel = require('../api/models/list.model')
 const TaskModel = require('../api/models/task.model')
-const AssignedUsers = require("../api/models/assignedUser.model")
+//const AssignedUsers = require("../api/models/assignedUser.model")
+//const RegistryTask = require("../api/models/registryTask.model")
 
 const addRelationships = () => {
     try {
-        UserModel.belongsToMany(ListModel, { through: "AssignedUsers" })
-        ListModel.belongsToMany(UserModel, { through:" AssignedUsers" })
+        //UserModel.belongsToMany(ListModel, { through: AssignedUsers })
+        //ListModel.belongsToMany(UserModel, { through: AssignedUsers })
 
-       UserModel.hasMany(ListModel)
-       ListModel.belongsTo(UserModel)
+        //AssignedUsers.belongsToMany(TaskModel, { through: RegistryTask })
+        //TaskModel.belongsToMany(AssignedUsers, { through: RegistryTask })
+
+        UserModel.hasMany(ListModel)
+        ListModel.belongsTo(UserModel)
 
         ListModel.hasMany(TaskModel)
         TaskModel.belongsTo(ListModel)
--- db/sync.js
+++ db/sync.js
@@ -2,14 +2,15 @@ const UserModel = require('../api/models/user.model')
 const ListModel = require('../api/models/list.model')
 const TaskModel = require('../api/models/task.model')
 const AssignedUsers = require("../api/models/assignedUser.model")
-
+const RegistryTask = require('../api/models/registryTask.model')
 
 const dbSync = async () => {
     try {
         await UserModel.sync({alter: true})
         await ListModel.sync({alter: true})
         await TaskModel.sync({alter: true})
-        await AssignedUsers.sync()
+        await AssignedUsers.sync({alter: true})
+        await RegistryTask.sync({alter: true})
     } catch (error) {
         console.log(error)
         throw new Error(error)
