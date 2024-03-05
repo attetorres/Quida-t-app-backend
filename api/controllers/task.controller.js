3496affd4030007f1839b77e9340e098641d3f2f
Fran Arteaga <fran.am8@gmail.com>
2024-03-05 15:21:28 +0000

    TASK: controllers working: createTask and deleteTask Done! getTasks & updateTasks missing (task belongs to list belongs to user == res.locals.user) condition

--- api/controllers/task.controller.js
+++ api/controllers/task.controller.js
@@ -1,9 +1,22 @@
 const TaskModel = require('../models/task.model')
+const ListModel = require('../models/list.model')
+const UserModel = require('../models/user.model')
 
 const createTask = async (req, res) => {
     try {
-        const task = await TaskModel.create(req.body)
-        res.status(200).json({ message: 'Task created', task: task })
+
+        const list = await ListModel.findByPk(req.params.listId)
+
+        const user = await UserModel.findByPk(list.creator_user_id)
+
+        if (user.email === res.locals.user.email){
+            const task = await TaskModel.create(req.body)
+            res.status(200).json({ message: 'Task created', task: task })
+
+        } else {
+            res.statu(500).send('You are not the creator of the list')
+        }
+       
 
     } catch (error) {
         console.log(error)
@@ -57,11 +70,26 @@ const deleteTask = async (req, res) => {
         // we need taskId
         // compare the column listId from said taskiD
         // compare userId of colum creator from said listID
-        // compre userId/userEmail with userId/userEmail from token?
-        const task = await TaskModel.destroy(req.params.taskId)
-        if (!task) return res.status(500).send('No task found')
+        const task = await TaskModel.findByPk(req.params.taskId)
 
+        const list = await ListModel.findByPk(task.listId)
+
+        const user = await UserModel.findByPk(list.creator_user_id)
+
+        // compre userId/userEmail from taskId with userId/userEmail from token?
+
+
+        if (user.email === res.locals.user.email) {
+            await TaskModel.destroy({
+                where: {
+                    id: req.params.taskId}
+                })
+
+        if (!task) return res.status(500).send('No task found')
+        
         res.status(200).send('Task deleted successfully')
+        }
+        
     } catch (error) {
         console.log(error)
         res.status(500).send('Error deleting the task')
