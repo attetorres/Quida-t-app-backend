747d0e8616c0e1f266857c4d22255a8d2446a74a
Aitor Díaz Santana <aitordd.ss@gmail.com>
2024-03-06 10:43:27 +0000

    assignedUsers and registryTask changes in model and sync

--- db/relationships.js
+++ db/relationships.js
@@ -1,20 +1,24 @@
 const UserModel = require('../api/models/user.model')
 const ListModel = require('../api/models/list.model')
 const TaskModel = require('../api/models/task.model')
-//const AssignedUsers = require("../api/models/assignedUser.model")
-//const RegistryTask = require("../api/models/registryTask.model")
+const AssignedUsers = require("../api/models/assignedUser.model")
+const RegistryTask = require("../api/models/registryTask.model")
 
 const addRelationships = () => {
     try {
-        //UserModel.belongsToMany(ListModel, { through: AssignedUsers })
-        //ListModel.belongsToMany(UserModel, { through: AssignedUsers })
+       
+        UserModel.belongsToMany(ListModel, { through: AssignedUsers })
+        ListModel.belongsToMany(UserModel, { through: AssignedUsers })
+        
+        TaskModel.hasMany(RegistryTask)
+        RegistryTask.belongsTo(TaskModel)
 
-        //AssignedUsers.belongsToMany(TaskModel, { through: RegistryTask })
-        //TaskModel.belongsToMany(AssignedUsers, { through: RegistryTask })
-
+        AssignedUsers.hasMany(RegistryTask)
+        RegistryTask.belongsTo(AssignedUsers)
+        
         UserModel.hasMany(ListModel)
         ListModel.belongsTo(UserModel)
-
+        
         ListModel.hasMany(TaskModel)
         TaskModel.belongsTo(ListModel)
         
